5fc58d8c8ff554e7177d65f08c4e3573a68c226a
Aisha Mohammed <80531561+aisha-rm@users.noreply.github.com>
2022-10-27 22:44:28 +0100

    new files

---
+++ src/main/java/com/tmt/theapitask/TheApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tmt.theapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheApiTaskApplication.class, args);
+	}
+
+}
